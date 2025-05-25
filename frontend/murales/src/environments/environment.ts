561f10ccddd72903fc32fc413e5bdfde7911e920
Alejandor Daniel López Orona <128829840+Daniel-orona21@users.noreply.github.com>
2025-05-24 22:17:50 -0600

    peticiones a back desplegado

--- frontend/murales/src/environments/environment.ts
+++ frontend/murales/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api',
-  socketUrl: 'http://localhost:3000'
-  // apiUrl: 'https://murales.onrender.com/api',
-  // socketUrl: 'https://murales.onrender.com'
+  // apiUrl: 'http://localhost:3000/api',
+  // socketUrl: 'http://localhost:3000'
+  apiUrl: 'https://murales.onrender.com/api',
+  socketUrl: 'https://murales.onrender.com'
 }; 
