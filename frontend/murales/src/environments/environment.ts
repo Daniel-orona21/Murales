7870635dc8430756fa081e6ed67c36deedbd060c
Alejandor Daniel López Orona <128829840+Daniel-orona21@users.noreply.github.com>
2025-05-27 12:36:52 -0600

    sombra texto cargando

--- frontend/murales/src/environments/environment.ts
+++ frontend/murales/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  // apiUrl: 'http://192.168.1.12:3000/api',
-  // socketUrl: 'http://192.168.1.12:3000'
-  apiUrl: 'https://murales.onrender.com/api',
-  socketUrl: 'https://murales.onrender.com'
+  apiUrl: 'http://192.168.1.12:3000/api',
+  socketUrl: 'http://192.168.1.12:3000'
+  // apiUrl: 'https://murales.onrender.com/api',
+  // socketUrl: 'https://murales.onrender.com'
 }; 
